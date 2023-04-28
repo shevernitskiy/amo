3e6ce24f47b97bc648131c8ac5aa4ed542bebddf
shevernitskiy <mahahuha@ya.ru>
2023-04-28 09:36:52 +0300

    expose more things in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,17 @@
 export { Amo } from "./src/amo.ts";
+
+export * from "./src/typings/auth.ts";
+export * from "./src/typings/entities.ts";
+export * from "./src/typings/lib.ts";
+export * from "./src/typings/utility.ts";
+
+export { ApiError } from "./src/errors/api.ts";
+export { AuthError } from "./src/errors/auth.ts";
+export { HttpError } from "./src/errors/http.ts";
+export { NoContentError } from "./src/errors/no-content.ts";
+export { WebhookError } from "./src/errors/webhook.ts";
+
+export * from "./src/helpers/filter.ts";
+export * from "./src/helpers/order.ts";
+export * from "./src/helpers/query.ts";
+export * from "./src/helpers/webhook.ts";
