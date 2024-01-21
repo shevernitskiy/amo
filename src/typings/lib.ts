e61d2e2e3c3265717a02f77298305464987fea18
shevernitskiy <mahahuha@ya.ru>
2024-01-21 12:28:36 +0300

    add default error handler

--- src/typings/lib.ts
+++ src/typings/lib.ts
@@ -1,9 +1,15 @@
+import { AuthError } from "../errors/auth.ts";
+import { NoContentError } from "../errors/no-content.ts";
+import { HttpError } from "../errors/http.ts";
+import { WebhookError } from "../errors/webhook.ts";
+import { ApiError } from "../errors/api.ts";
 import { OAuth } from "./auth.ts";
 import { JSONValue } from "./utility.ts";
 
 export type Options = {
   request_delay?: number;
   on_token?: (token: OAuth) => void | Promise<void>;
+  on_error?: (error: Error | AuthError | ApiError | NoContentError | HttpError | WebhookError) => void | Promise<void>;
 };
 
 export type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
