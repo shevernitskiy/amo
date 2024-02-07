a81d99e703ce81f9ac9e9bc24dd7fd644498265e
shevernitskiy <mahahuha@ya.ru>
2024-02-07 08:41:00 +0300

    add `ConcurrentPool`

--- src/typings/lib.ts
+++ src/typings/lib.ts
@@ -1,13 +1,16 @@
-import { AuthError } from "../errors/auth.ts";
-import { NoContentError } from "../errors/no-content.ts";
-import { HttpError } from "../errors/http.ts";
-import { WebhookError } from "../errors/webhook.ts";
-import { ApiError } from "../errors/api.ts";
 import { OAuth } from "./auth.ts";
+import { ApiError } from "../errors/api.ts";
+import { AuthError } from "../errors/auth.ts";
+import { HttpError } from "../errors/http.ts";
+import { NoContentError } from "../errors/no-content.ts";
+import { WebhookError } from "../errors/webhook.ts";
+
 import { JSONValue } from "./utility.ts";
 
 export type Options = {
   request_delay?: number;
+  concurrent_request?: number;
+  concurrent_timeframe?: number;
   on_token?: (token: OAuth) => void | Promise<void>;
   on_error?: (error: Error | AuthError | ApiError | NoContentError | HttpError | WebhookError) => void | Promise<void>;
 };
